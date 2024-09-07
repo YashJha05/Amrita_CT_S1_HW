471371cf55a08a47d6d7ec4e79069d0e701e5910
Yash Jha <72140357+YashJha05@users.noreply.github.com>
2024-09-07 17:53:46 +0530

    Create for_loop_assignment.py

---
+++ for_loop_assignment.py
@@ -0,0 +1,28 @@
+# Write a 'for loop' that iterates over a list of strings and prints each string in uppercase.
+
+l = ["a", "b", "c", "d"]
+for i in l:
+  print(i.upper())
+
+# Write a 'for loop' that prints the square of each number in a given list.
+
+l = [1, 2, 3, 4, 5, 6]
+for i in l:
+  print(i**2)
+
+# write a nested 'for loop' to print the transpose of a 2x3 matrix.
+
+# couldn't solve
+
+# Use a 'for loop' with 'range()' to print the first 10 even numbers.
+
+x = 0
+for i in range(10):
+  print(x)
+  x += 2
+
+# Write a 'for loop' that iterates over a dictionary and prints each key-value pair.
+
+x = {'a': 1, 'b': 2, 'c': 3, 'd':4}
+for i in x.items():
+    print(i)
